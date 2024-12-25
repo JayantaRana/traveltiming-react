698826bde46d5954c7f9f02691c903a124255ecd
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2024-12-25 22:50:23 +0530

    Update busRoutes.js

--- routes/busRoutes.js
+++ routes/busRoutes.js
@@ -2,27 +2,35 @@ const express = require('express');
 const router = express.Router();
 const Bus = require('../models/Bus');
 
-// Get all buses
-router.get('/', async (req, res) => {
-  try {
-    const buses = await Bus.find();
-    res.json(buses);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
-// Search buses by source and destination
 router.get('/search', async (req, res) => {
   const { source, destination } = req.query;
 
   try {
-    const buses = await Bus.find({
-      'stops.name': { $all: [source, destination] },
-    });
-    
+    const buses = await Bus.aggregate([
+      { $unwind: '$stops' },
+      {
+        $match: {
+          'stops.name': { $in: [source, destination] },
+        },
+      },
+      {
+        $group: {
+          _id: '$bus_name',
+          stops: { $push: '$stops' },
+          phone: { $first: '$phone' },
+        },
+      },
+      {
+        $project: {
+          bus_name: '$_id',
+          stops: 1,
+          phone: 1,
+          _id: 0,
+        },
+      },
+    ]);
 
-    // Filter to ensure source comes before destination
     const filteredBuses = buses.filter((bus) => {
       const stops = bus.stops.map((stop) => stop.name);
       return stops.indexOf(source) < stops.indexOf(destination);
@@ -33,5 +41,41 @@ router.get('/search', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
 module.exports = router;
+
+
+
+
+// // Get all buses
+// router.get('/', async (req, res) => {
+//   try {
+//     const buses = await Bus.find();
+//     res.json(buses);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+// // Search buses by source and destination
+// router.get('/search', async (req, res) => {
+//   const { source, destination } = req.query;
+
+//   try {
+//     const buses = await Bus.find({
+//       'stops.name': { $all: [source, destination] },
+//     });
+    
+
+//     // Filter to ensure source comes before destination
+//     const filteredBuses = buses.filter((bus) => {
+//       const stops = bus.stops.map((stop) => stop.name);
+//       return stops.indexOf(source) < stops.indexOf(destination);
+//     });
+
+//     res.json(filteredBuses);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+// module.exports = router;

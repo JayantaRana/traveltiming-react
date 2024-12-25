e690fd9c60a03dd524f485ce7629b900a6387db7
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2024-12-25 22:47:25 +0530

    Update busRoutes.js

--- routes/busRoutes.js
+++ routes/busRoutes.js
@@ -14,14 +14,13 @@ router.get('/', async (req, res) => {
 
 // Search buses by source and destination
 router.get('/search', async (req, res) => {
-  // const { source, destination } = req.query;
-const { source, destination, page = 1, limit = 10 } = req.query;  // Default to page 1 and limit 10 buses
+  const { source, destination } = req.query;
+
   try {
     const buses = await Bus.find({
       'stops.name': { $all: [source, destination] },
-    })
-     .skip((page - 1) * limit) // Pagination: skip the previous pages
-    .limit(parseInt(limit))   // Limit the number of buses returned
+    });
+    
 
     // Filter to ensure source comes before destination
     const filteredBuses = buses.filter((bus) => {

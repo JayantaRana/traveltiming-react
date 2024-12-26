d743008f110e420555caff695229cade3bd2e262
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2024-12-26 08:53:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,serverSelectionTimeoutMS: 30000 })
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 

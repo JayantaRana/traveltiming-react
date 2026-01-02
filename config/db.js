ec68bdc5dd22c48efcb55b1ea96a8ce33bb88876
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2026-01-02 12:18:08 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+// BUS DATABASE
+const busDB = mongoose.createConnection(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// POPUP DATABASE
+const popupDB = mongoose.createConnection(
+  process.env.MONGO_POPUP_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+busDB.on("connected", () => console.log("✅ Bus DB connected"));
+popupDB.on("connected", () => console.log("✅ Popup DB connected"));
+
+busDB.on("error", err => console.error("Bus DB error:", err));
+popupDB.on("error", err => console.error("Popup DB error:", err));
+
+module.exports = { busDB, popupDB };

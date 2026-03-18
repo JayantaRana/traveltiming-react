3e6a338079fd76a741ed4010bbe345cd7ce7dc7e
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2026-03-18 16:28:08 +0530

    Create AdminLog model for admin actions logging

---
+++ models/AdminLog.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const AdminLogSchema = new mongoose.Schema({
+
+  admin:{
+    type:String
+  },
+
+  action:{
+    type:String
+  },
+
+  busname:{
+    type:String
+  },
+
+  time:{
+    type:Date,
+    default:Date.now
+  }
+
+});
+
+module.exports = mongoose.model("AdminLog",AdminLogSchema);

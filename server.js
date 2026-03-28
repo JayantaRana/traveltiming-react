88a05085c4b87c4a725a1e5a51ca687210c9c512
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2026-03-28 11:39:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const busRoutes = require('./routes/busRoutes'); // Import the routes
 
 const popupRoutes = require('./routes/popupRoutes');//new add for popup📌
-const busToggleRoutes = require("./routes/busToggle.routes"); //📍add for togglecount
+
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
@@ -28,7 +28,7 @@ app.use(express.json());
 // Routes
 app.use('/buses', busRoutes); // All routes related to buses
 app.use('/api/popups', popupRoutes);//new add for popup📌
-app.use("/api/bus-toggle", busToggleRoutes);//📍add for togglecount
+
 
 
 // Root Route for Health Check 

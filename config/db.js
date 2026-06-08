ffb8b1f176d06f12d54ed101de78c51db9a40628
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2026-06-08 20:34:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,16 +9,17 @@ const busDB = mongoose.createConnection(
 );
 
 // POPUP DATABASE
-const popupDB = mongoose.createConnection(
-  process.env.MONGO_POPUP_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+// const popupDB = mongoose.createConnection(
+//   process.env.MONGO_POPUP_URI,
+//   { useNewUrlParser: true, useUnifiedTopology: true }
+// );
 
 busDB.on("connected", () => console.log("✅ Bus DB connected"));
-popupDB.on("connected", () => console.log("✅ Popup DB connected"));
+// popupDB.on("connected", () => console.log("✅ Popup DB connected"));
 
 busDB.on("error", err => console.error("Bus DB error:", err));
-popupDB.on("error", err => console.error("Popup DB error:", err));
+// popupDB.on("error", err => console.error("Popup DB error:", err));
 
-module.exports = { busDB, popupDB };
+// module.exports = { busDB, popupDB };
+module.exports = {busDB}
 

55b26f9411453405fce725db430209837a1e1c7e
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2026-06-08 20:32:13 +0530

    Update Bus.js

--- models/Bus.js
+++ models/Bus.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const { busDB } = require("../config/db");//new add for popup
+const { busDB } = require("../config/db");//new add for popup
 
 const BusSchema = new mongoose.Schema({
   busname: { type: String, required: true },
